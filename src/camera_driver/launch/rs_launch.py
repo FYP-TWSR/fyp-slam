3b9f4dd1154a3531809dbf3109bcf18ca9a2f668
Mihawk <hhuangbl@connect.ust.gk>
2023-09-19 19:43:26 +0800

    finish migrate orb slam3

--- src/camera_driver/launch/rs_launch.py
+++ src/camera_driver/launch/rs_launch.py
@@ -50,8 +50,8 @@ configurable_parameters = [{'name': 'camera_name',                  'default': '
                            {'name': 'pointcloud.enable',            'default': 'false', 'description': ''}, 
                            {'name': 'pointcloud.stream_filter',     'default': '2', 'description': 'texture stream for pointcloud'},
                            {'name': 'pointcloud.stream_index_filter','default': '0', 'description': 'texture stream index for pointcloud'},
-                           {'name': 'enable_sync',                  'default': 'false', 'description': "''"},                           
-                           {'name': 'align_depth.enable',           'default': 'false', 'description': "''"},                           
+                           {'name': 'enable_sync',                  'default': 'true', 'description': "''"},                           
+                           {'name': 'align_depth.enable',           'default': 'true', 'description': "''"},                           
                            {'name': 'colorizer.enable',             'default': 'false', 'description': "''"},
                            {'name': 'clip_distance',                'default': '-2.', 'description': "''"},                           
                            {'name': 'linear_accel_cov',             'default': '0.01', 'description': "''"},                           
@@ -73,6 +73,8 @@ configurable_parameters = [{'name': 'camera_name',                  'default': '
                            {'name': 'hdr_merge.enable',             'default': 'false', 'description': 'hdr_merge filter enablement flag'},
                            {'name': 'wait_for_device_timeout',      'default': '-1.', 'description': 'Timeout for waiting for device to connect (Seconds)'},
                            {'name': 'reconnect_timeout',            'default': '6.', 'description': 'Timeout(seconds) between consequtive reconnection attempts'},
+                           {'name': 'depth_module.profile',            'default': '640x480x30', 'description': 'hai de shi mihawk'},
+                           {'name': 'rgb_camera.profile',            'default': '640x480x30', 'description': 'hai de shi mihawk'},
                           ]
 
 def declare_configurable_parameters(parameters):
